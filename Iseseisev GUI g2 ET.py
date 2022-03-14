9accc5239587bf7a35eaaca373ffcd7492e09035
IDerik22 <98737025+IDerik22@users.noreply.github.com>
2022-03-14 13:53:06 +0200

    Add files via upload

---
+++ Iseseisev GUI g2 ET.py
@@ -0,0 +1,24 @@
+#Erik Teppan
+#IT-21
+#14.03
+import turtle
+for i in range (4):
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.forward(80)
+    turtle.left(-90)
+    turtle.forward(60)
+    turtle.left(-90)
+    turtle.forward(80)
+    turtle.left(90)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+turtle.exitonclick()
